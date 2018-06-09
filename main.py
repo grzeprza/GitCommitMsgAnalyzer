a8de0073c245e6d8ace1627fca820bb84704214b
grzeprza <gprzadka@interia.pl>
2018-06-09 22:03:42 +0200

    Create dedicated sections in main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 #own
 import FileUtils
 from StatUtils import drawHistogram
@@ -6,32 +5,52 @@ from StatUtils import analyzeComit
 from ProjectStats import ProjectStats
 file_path = FileUtils.chooseFile()
 
-#parses commits based on headers
-parsedCommits = FileUtils.parseFile(file_path)
+fileContent = ""
+parseCommitsFromFile = False
+genProjStats = False
+genCommitAnalysis = False
+saveResultToFile = False
 
-print("Succesfully parsed %d commits" % len(parsedCommits))
-#
-# analyzedCommits = []
-# for commit in parsedCommits:
-#     analyzedCommits.append(analyzeComit(commitToAnal=commit))
+# Parses commits based on headers =======================================================
+if(parseCommitsFromFile):
+    parsedCommits = FileUtils.parseFile(file_path)
 
-# for commitStat in analyzedCommits:
-#     print (commitStat.readible() + "\n")
+    print("Succesfully parsed %d commits" % len(parsedCommits))
+    fileContent += "Succesfully parsed %d commits\n\n" % len(parsedCommits)
+else:
+    print("\nParsing Commits form file skipped.\n")
 
-# print("Succesfully analyzed %d commits" % len(analyzedCommits))
+# Gather project statistics =============================================================
+if(genProjStats):
+    projStatsTitle = "Project statistics"
+    projStats = ProjectStats(parsedCommits)
+    projStats.countAvgCommitsPerUser()
+    projStats.countOneCommittersNumber()
+    projStats.countWordPerCommit()
+    # projStats.inspectUsedPartOfSpeech()
+    print("Authors count : %d" % len(projStats.getUniqueAuthorsDict())) #TODO: ON kubernetes github - 1693, excel - 1980, program - 1997
+    print("Commiters count : %d" % len(projStats.getUniqueCommitersDict())) #TODO: on kubernetes 1867
+    print(projStats)
+else:
+    print("\nGenerating general project statistics skipped.\n")
 
-projStats = ProjectStats(parsedCommits)
-projStats.countAvgCommitsPerUser()
-projStats.countOneCommittersNumber()
-projStats.countWordPerCommit()
-# projStats.inspectUsedPartOfSpeech()
+# Analyze commits =======================================================================
+if(genCommitAnalysis):
+    analyzedCommits = []
+    for commit in parsedCommits:
+        analyzedCommits.append(analyzeComit(commitToAnal=commit))
 
-print(projStats)
-# summaryFile = open("./summary.txt", "w")
-# for committs in parsedCommits:
-#     print(committs, end="", flush=True)
-#     summaryFile.write(str(committs))
-#     # print("\n")
-#
-# summaryFile.close()
+    for commitStat in analyzedCommits:
+        print (commitStat.readible() + "\n")
 
+    print("Succesfully analyzed %d commits" % len(analyzedCommits))
+else:
+    print("\nAnalyzing commits skipped.\n")
+
+# Save results to File ==================================================================
+if(saveResultToFile):
+    summaryFile = open("./summary.txt","w")
+    summaryFile.write(fileContent)
+    summaryFile.close()
+else:
+    print("\nSaving results to file skipped.\n")
