8f53c1c8c943a0be515fc5e8b5e0bad29da0f927
grzeprza <gprzadka@interia.pl>
2018-06-11 20:58:54 +0200

    Remove unused enum types

--- TxtFileHeaderEnum.py
+++ TxtFileHeaderEnum.py
@@ -1,11 +1,10 @@
 from enum import Enum #to create enum types
 
 class TxtFileHeader(Enum) :
-    KOMIT = "KOMIT",
-    ID = "ID",
-    AUTHOR = "AUTHOR",
-    COMMITTER = "COMMITTER",
-    SUBJECT = "SUBJECT",
-    BODY = "BODY",
-    ALL = "ALL"
+    ID = "ID:",
+    AUTHOR = "AUTHOR:",
+    COMMITTER = "COMMITTER:",
+    SUBJECT = "SUBJECT:",
+    BODY = "BODY:",
+    ALL = "ALL:"
 
